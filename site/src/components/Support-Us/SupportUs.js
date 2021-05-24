8983470c85e75bf84a65a9b1924632f364fb66a1
IanWach <iankaveli0@gmail.com>
2021-05-24 23:10:39 +0300

    Edited the Style;s File Location

--- site/src/components/Support-Us/SupportUs.js
+++ site/src/components/Support-Us/SupportUs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './SupportUs.css'
+import '../styles/SupportUs.css'
 
 function SupportUs(){
     return(
@@ -9,13 +9,13 @@ function SupportUs(){
                 <p>Your support is highly appreciated. The Christian union undertakes different projects that need funding. This include the <i>Welfare Kitty:</i> which collaborates in helping needy students, <i>Prison and Rescue center visits</i>: that are conducted to provide spiritual and material support to the needy. You can play part by supporting us in any way. Through prayers and financial contributions towards the same. Thank you.</p>
                 <button href="#">Learn More</button>
             </div>
-            <div className="Second-Section">
-                <hr/>
-                <p id="First-Paragraph"><b>Galatians 6:9</b>	Let us not become weary in doing good, for at the proper time we will reap a harvest if we do not give up.</p>
-                <br/>
-                <p id="Second-Paragraph"><b>6:10</b>	Therefore, as we have opportunity, let us do good to all people, especially to those who belong to the family of believers.</p>
-                <button>Support Us</button>
-                <hr/>
+                <div className="Second-Section">
+                    <hr/>
+                    <p id="First-Paragraph"><b>Galatians 6:9</b>	Let us not become weary in doing good, for at the proper time we will reap a harvest if we do not give up.</p>
+                    <br/>
+                    <p id="Second-Paragraph"><b>6:10</b>	Therefore, as we have opportunity, let us do good to all people, especially to those who belong to the family of believers.</p>
+                    <button>Support Us</button>
+                    <hr/>
             </div>
         </div>
     )
