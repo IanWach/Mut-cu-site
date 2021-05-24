ff90b5e7c44c9f0889ea515b958559c902f38be6
IanWach <iankaveli0@gmail.com>
2021-05-24 14:03:53 +0300

    I changed some CSS Properties

--- site/src/components/Support-Us/SupportUs.js
+++ site/src/components/Support-Us/SupportUs.js
@@ -6,13 +6,16 @@ function SupportUs(){
         <div className="SupportUs-Container" id="Screen-Size">
             <div className="First-Section">
                 <h1>Support Us</h1>
-                <p>Serving in the church is not something we should need a special invitation to do. In every church there will be needs to meet and Christians to disciple. Church involvement is evidence that you are a Christian in first place because it is an avenue for using your spiritual gift to help others.</p>
-                <button>Learn More</button>
+                <p>Your support is highly appreciated. The Christian union undertakes different projects that need funding. This include the <i>Welfare Kitty:</i> which collaborates in helping needy students, <i>Prison and Rescue center visits</i>: that are conducted to provide spiritual and material support to the needy. You can play part by supporting us in any way. Through prayers and financial contributions towards the same. Thank you.</p>
+                <button href="#">Learn More</button>
             </div>
             <div className="Second-Section">
+                <hr/>
                 <p id="First-Paragraph"><b>Galatians 6:9</b>	Let us not become weary in doing good, for at the proper time we will reap a harvest if we do not give up.</p>
                 <br/>
                 <p id="Second-Paragraph"><b>6:10</b>	Therefore, as we have opportunity, let us do good to all people, especially to those who belong to the family of believers.</p>
+                <button>Support Us</button>
+                <hr/>
             </div>
         </div>
     )
